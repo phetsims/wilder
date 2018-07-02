bbe8a14a0b0d171d0c2acb68c4ae6c320c32979a
Jonathan Olson <jonathan.olson@colorado.edu>
2018-07-02 16:08:37 -0600

    Additional ES6 bits and assertion failures on fully-built sims

--- js/wilder/model/WilderModel.js
+++ js/wilder/model/WilderModel.js
@@ -20,72 +20,84 @@ define( function( require ) {
    * @constructor
    */
   function WilderModel() {
+    // We want a built version to error out for these "asserts"
+    function hardAssert( condition, message = '' ) {
+      if ( !condition ) {
+        throw new Error( message );
+      }
+    }
+
     // block scoping for let/const
     let blocky = 'outside';
     if ( window ) {
       let blocky = 'inside';
-      assert && assert( blocky === 'inside' );
+      hardAssert( blocky === 'inside' );
     }
     for ( let blocky = 0; blocky <= 0; blocky++ ) {
-      assert && assert( blocky === 0 );
+      hardAssert( blocky === 0 );
     }
-    assert && assert( blocky === 'outside' );
+    hardAssert( blocky === 'outside' );
 
     // Expression-based arrow functions
-    assert && assert( _.range( 0, 5 ).map( x => x * x )[ 3 ] === 9 );
+    hardAssert( _.range( 0, 5 ).map( x => x * x )[ 3 ] === 9 );
 
     // Statement-based arrow functions
-    assert && assert( _.range( 0, 5 ).map( x => {
-      assert && assert( x < 5 );
+    hardAssert( _.range( 0, 5 ).map( x => {
+      hardAssert( x < 5 );
       return x * x;
     } )[ 3 ] === 9 );
 
     var self = this;
     [ 1 ].map( element => {
-      assert && assert( this === self );
+      hardAssert( this === self );
     } );
 
     // Default function parameters
     function defaults( x = 1, y = 2, z = 3 ) {
       return x + y + z;
     }
-    assert && assert( defaults( 0 ) === 5 );
+    hardAssert( defaults( 0 ) === 5 );
 
     // Rest parameters
     function rest( x, y, ...others ) {
       return x + y + others.length;
     }
-    assert && assert( rest( 1, 2, 3, 4, 5, 6 ) === 7 );
+    hardAssert( rest( 1, 2, 3, 4, 5, 6 ) === 7 );
 
     // Spread operator
     const constArray = [ 1, 2, 3 ];
-    assert && assert( [ ...constArray, 4, 5, ...constArray ].length === 8 );
-    assert && assert( rest( 4, ...constArray ) === 7 );
+    hardAssert( [ ...constArray, 4, 5, ...constArray ].length === 8 );
+    hardAssert( rest( 4, ...constArray ) === 7 );
 
     // String interpolation
-    assert && assert( `Testing ${2 + 3}` === 'Testing 5' );
+    hardAssert( `Testing ${2 + 3}` === 'Testing 5' );
 
     // Custom interpolation
     function quoter( strings, ...quotations ) {
       return interleave( strings, i => `"${quotations[ i ]}"` ).join( '' );
     }
-    assert && assert( quoter`He said ${something} but then answered ${3*2}` === 'He said "foo" but then answered "6"' );
+    hardAssert( quoter`He said ${something} but then answered ${3*2}` === 'He said "foo" but then answered "6"' );
+
+    const multiLineString = `This
+ is a test of the emergency
+ newline system`;
+    hardAssert( multiLineString.length === 48 );
 
     // Binary
-    assert && assert( 0b111 === 3 + 4 );
+    hardAssert( 0b111 === 3 + 4 );
 
     // Object shorthand
     const a = 5;
     const b = 4;
     const shortObj = { a, b };
-    assert && assert( shortObj.a === a );
-    assert && assert( shortObj.b === b );
+    hardAssert( shortObj.a === a );
+    hardAssert( shortObj.b === b );
 
     // Computed property names
     const computedObj = {
       [ something ]: a
     };
-    assert && assert( computedObj[ something ] === a );
+    hardAssert( computedObj[ something ] === a );
 
     // Method notation
     const methodObj = {
@@ -93,13 +105,20 @@ define( function( require ) {
         return a + b;
       }
     };
-    assert && assert( methodObj.add( 1, 2 ) === 3 );
+    hardAssert( methodObj.add( 1, 2 ) === 3 );
 
     // Array destructuring
     const arrList = [ 1, 2, 3 ];
     const [ firstElement, , thirdElement ] = arrList;
-    assert && assert( firstElement === arrList[ 0 ] );
-    assert && assert( thirdElement === arrList[ 2 ] );
+    hardAssert( firstElement === arrList[ 0 ] );
+    hardAssert( thirdElement === arrList[ 2 ] );
+
+    // Swapping with destructuring
+    let arf = 5;
+    let woof = 10;
+    [ arf, woof ] = [ woof, arf ];
+    hardAssert( arf === 10 );
+    hardAssert( woof === 5 );
 
     // Object destructuring
     const destObject = {
@@ -110,21 +129,34 @@ define( function( require ) {
       bird: 'canary'
     };
     const { cat, bird } = destObject;
-    assert && assert( cat === destObject.cat );
-    assert && assert( bird === destObject.bird );
+    hardAssert( cat === destObject.cat );
+    hardAssert( bird === destObject.bird );
     const { cat: tabby } = destObject;
-    assert && assert( tabby === destObject.cat );
+    hardAssert( tabby === destObject.cat );
     const { mouse: { animals } } = destObject;
-    assert && assert( animals === destObject.mouse.animals );
+    hardAssert( animals === destObject.mouse.animals );
     const { unmatched = 'default' } = destObject;
-    assert && assert( unmatched === 'default' );
+    hardAssert( unmatched === 'default' );
 
     // Parameter destructuring
     function destruct( { cat, mouse: { animals: [ firstAnimal ] } } ) {
       return cat + firstAnimal;
     }
-    assert && assert( destruct( destObject ) === destObject.cat + destObject.mouse.animals[ 0 ] );
+    hardAssert( destruct( destObject ) === destObject.cat + destObject.mouse.animals[ 0 ] );
 
+    // Options object destructuring with defaults
+    const optionsObject = {
+      tree: 4,
+      forest: 5
+    };
+    const {
+      tree = 12,
+      forest = 100,
+      leaf = 1024
+    } = optionsObject || {};
+    hardAssert( tree === 4 );
+    hardAssert( forest === 5 );
+    hardAssert( leaf === 1024 );
 
     // Class extending current hierarchy
     const MUTATOR_KEYS = [ ...Node.prototype._mutatorKeys, 'secret' ];
@@ -151,9 +183,9 @@ define( function( require ) {
 
       static createSecretNode() { return new SecretNode( { secret: 0 } ); }
     }
-    assert && assert( new SecretNode( { secret: 5 } ).secret === 5 );
-    assert && assert( new SecretNode( { opacity: 0.5 } ).opacity === 0.5 );
-    assert && assert( SecretNode.createSecretNode().secret === 0 );
+    hardAssert( new SecretNode( { secret: 5 } ).secret === 5 );
+    hardAssert( new SecretNode( { opacity: 0.5 } ).opacity === 0.5 );
+    hardAssert( SecretNode.createSecretNode().secret === 0 );
 
     // Iterable class (with a generator method)
     class RelativePrimes {
@@ -175,21 +207,21 @@ define( function( require ) {
       if ( n >= 12 ) { break; }
       relativePrimes.push( n );
     }
-    assert && assert( _.isEqual( relativePrimes, [ 1, 2, 3, 4, 6, 7, 8, 9, 11 ] ) );
+    hardAssert( _.isEqual( relativePrimes, [ 1, 2, 3, 4, 6, 7, 8, 9, 11 ] ) );
 
     // Sets
     const bag = new Set();
     bag.add( 'a' ).add( 'b' ).add( 'a' );
-    assert && assert( bag.size === 2 );
-    assert && assert( bag.has( 'a' ) );
-    assert && assert( !bag.has( 'c' ) );
+    hardAssert( bag.size === 2 );
+    hardAssert( bag.has( 'a' ) );
+    hardAssert( !bag.has( 'c' ) );
 
     // Maps
     const map = new Map();
     map.set( bag, 5 );
     map.set( Node, 2 );
-    assert && assert( map.get( bag ) === 5 );
-    assert && assert( map.get( Node ) === 2 );
+    hardAssert( map.get( bag ) === 5 );
+    hardAssert( map.get( Node ) === 2 );
 
     // TODO: generator function method notation fun*(...){}
     // TODO: modules?
