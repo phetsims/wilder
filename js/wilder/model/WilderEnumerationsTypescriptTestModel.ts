a86433c09296bc1dd115c120c42341b582a8b715
Sam Reid <reids@colorado.edu>
2021-12-10 17:30:56 -0700

    Move StringEnumerationProperty.ts to axon, see https://github.com/phetsims/axon/issues/372

--- js/wilder/model/WilderEnumerationsTypescriptTestModel.ts
+++ js/wilder/model/WilderEnumerationsTypescriptTestModel.ts
@@ -9,7 +9,7 @@
 
 import Property from '../../../../axon/js/Property.js';
 import wilder from '../../wilder.js';
-import StringEnumerationProperty from './StringEnumerationProperty.js';
+import StringEnumerationProperty from '../../../../axon/js/StringEnumerationProperty.js';
 import RichEnumerationProperty from './RichEnumerationProperty.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
 
