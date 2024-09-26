092cee50aa6d1dac4029b47f9a4d3b4e2a4be550
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:10:39 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for wilder.
+ * ESLint configuration for wilder.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
